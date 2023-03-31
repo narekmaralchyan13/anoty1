2382d88652e128ea2d811312cb7451161011e4f5
narekmaralchyan13 <narek@ultralabs.io>
2023-03-31 19:12:29 +0400

    init react moment

--- src/components/dateSelector/index.js
+++ src/components/dateSelector/index.js
@@ -1,6 +1,19 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import * as styles from './dateSelector.module.css'
 import SelectContainer from './../form/components/selectContainer'
+import moment from 'moment'
+
+function getDaysInMonth(year, month) {
+    const dates = [];
+    const date = moment(`${year}-${month}-01`);
+  
+    while (date.month() === parseInt(month) - 1) {
+      dates.push(date.format('ddd MMM DD YYYY'));
+      date.add(1, 'day');
+    }
+  
+    return dates;
+  }
 
 const times = [
     {
@@ -53,7 +66,23 @@ const times = [
 ]
 
 const DateSeletcor = ({selectTime,selectDay,selectTimeMode}) => {
-
+            
+    useEffect(()=>{
+        const years = []
+        const months = moment.months()
+        const currentYear = moment().year()
+        for(let i=0 ; i <5 ; i++){
+            years.push(currentYear+i)
+        }
+      
+        console.log(getDaysInMonth(2023,2))
+        
+    },[])
+    // const [date,setDate] = useState({
+    //     year: ,
+    //     month: ,
+    //     day: 
+    // })
     const [timesState, setTimesState] = useState(times)
 
     function changeTime(time) {
@@ -82,8 +111,24 @@ const DateSeletcor = ({selectTime,selectDay,selectTimeMode}) => {
     return (
         <SelectContainer>
             <div className={styles.dayContainer}>
-                <span className={styles.inputName}>Date *</span>
-                <input type='date' onChange={changeDay}  />
+                {/* <span className={styles.inputName}>Date *</span>
+                <input type='date' onChange={changeDay}  /> */}
+                <div className={styles.dateHeader}>
+                    <span className={styles.inputName}>Date *</span>
+                    <div className={styles.monthYear}>
+                        <select name='month' className={styles.timePeriod} >
+                            <option value='PM'>PM</option>
+                            <option value='AM'>AM</option>
+                        </select>
+                        <select name='timeMode' className={styles.timePeriod} >
+                            <option value='PM'>PM</option>
+                            <option value='AM'>AM</option>
+                        </select>
+                    </div>
+                </div>
+                <div>
+
+                </div>
             </div>
             <div className={styles.timeContainer}>
                 <div className={styles.timeHead}>
